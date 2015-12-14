5e5c1db76119db9f8c36b93ef118741b17726cf4
Julien Fontanet <julien.fontanet@isonoe.net>
2015-12-14 12:59:02 +0100

    Do not use babel-runtime to be as small as possible.

--- .mocha.js
+++ .mocha.js
@@ -3,3 +3,5 @@ Error.stackTraceLimit = 100
 try { require('trace') } catch (_) {}
 try { require('clarify') } catch (_) {}
 try { require('source-map-support/register') } catch (_) {}
+
+require('babel/polyfill')
