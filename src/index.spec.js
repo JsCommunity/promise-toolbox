b26d8c133ef108ca6a5c38abbe7a0bc7f11dea83
Julien Fontanet <julien.fontanet@isonoe.net>
2015-12-31 11:54:26 +0100

    Do not use babel-polyfill for testing.

--- src/index.spec.js
+++ src/index.spec.js
@@ -12,99 +12,95 @@ import {
 // ===================================================================
 
 describe('lastly()', () => {
-  it('calls a callback on resolution', async () => {
+  it('calls a callback on resolution', () => {
     const value = {}
     const spy = sinon.spy()
 
-    await expect(
+    return expect(
       AnyPromise.resolve(value)::lastly(spy)
     ).to.resolve.to.equal(
       value
-    )
-
-    expect(spy.callCount).to.equal(1)
+    ).then(() => {
+      expect(spy.callCount).to.equal(1)
+    })
   })
 
-  it('calls a callback on rejection', async () => {
+  it('calls a callback on rejection', () => {
     const reason = {}
     const spy = sinon.spy()
 
-    await expect(
+    return expect(
       AnyPromise.reject(reason)::lastly(spy)
     ).to.reject.to.equal(
       reason
-    )
-
-    expect(spy.callCount).to.equal(1)
+    ).then(() => {
+      expect(spy.callCount).to.equal(1)
+    })
   })
 })
 
 // -------------------------------------------------------------------
 
 describe('settle()', () => {
-  it('works with arrays', async () => {
-    const [
-      status1,
-      status2,
-      status3
-    ] = await settle([
+  it('works with arrays', () => {
+    return settle([
       AnyPromise.resolve(42),
       Math.PI,
       AnyPromise.reject('fatality')
-    ])
+    ]).then(([ status1, status2, status3 ]) => {
+      expect(status1.isFulfilled()).to.equal(true)
+      expect(status2.isFulfilled()).to.equal(true)
+      expect(status3.isFulfilled()).to.equal(false)
 
-    expect(status1.isFulfilled()).to.equal(true)
-    expect(status2.isFulfilled()).to.equal(true)
-    expect(status3.isFulfilled()).to.equal(false)
+      // Alias.
+      expect(status1.isResolved()).to.equal(true)
+      expect(status2.isResolved()).to.equal(true)
+      expect(status3.isResolved()).to.equal(false)
 
-    // Alias.
-    expect(status1.isResolved()).to.equal(true)
-    expect(status2.isResolved()).to.equal(true)
-    expect(status3.isResolved()).to.equal(false)
+      expect(status1.isRejected()).to.equal(false)
+      expect(status2.isRejected()).to.equal(false)
+      expect(status3.isRejected()).to.equal(true)
 
-    expect(status1.isRejected()).to.equal(false)
-    expect(status2.isRejected()).to.equal(false)
-    expect(status3.isRejected()).to.equal(true)
+      expect(status1.value()).to.equal(42)
+      expect(status2.value()).to.equal(Math.PI)
+      expect(::status3.value).to.throw()
 
-    expect(status1.value()).to.equal(42)
-    expect(status2.value()).to.equal(Math.PI)
-    expect(::status3.value).to.throw()
-
-    expect(::status1.reason).to.throw()
-    expect(::status2.reason).to.throw()
-    expect(status3.reason()).to.equal('fatality')
+      expect(::status1.reason).to.throw()
+      expect(::status2.reason).to.throw()
+      expect(status3.reason()).to.equal('fatality')
+    })
   })
 
-  it('works with objects', async () => {
-    const {
-      a: status1,
-      b: status2,
-      c: status3
-    } = await settle({
+  it('works with objects', () => {
+    return settle({
       a: AnyPromise.resolve(42),
       b: Math.PI,
       c: AnyPromise.reject('fatality')
+    }).then(({
+      a: status1,
+      b: status2,
+      c: status3
+    }) => {
+      expect(status1.isFulfilled()).to.equal(true)
+      expect(status2.isFulfilled()).to.equal(true)
+      expect(status3.isFulfilled()).to.equal(false)
+
+      // Alias.
+      expect(status1.isResolved()).to.equal(true)
+      expect(status2.isResolved()).to.equal(true)
+      expect(status3.isResolved()).to.equal(false)
+
+      expect(status1.isRejected()).to.equal(false)
+      expect(status2.isRejected()).to.equal(false)
+      expect(status3.isRejected()).to.equal(true)
+
+      expect(status1.value()).to.equal(42)
+      expect(status2.value()).to.equal(Math.PI)
+      expect(::status3.value).to.throw()
+
+      expect(::status1.reason).to.throw()
+      expect(::status2.reason).to.throw()
+      expect(status3.reason()).to.equal('fatality')
     })
-
-    expect(status1.isFulfilled()).to.equal(true)
-    expect(status2.isFulfilled()).to.equal(true)
-    expect(status3.isFulfilled()).to.equal(false)
-
-    // Alias.
-    expect(status1.isResolved()).to.equal(true)
-    expect(status2.isResolved()).to.equal(true)
-    expect(status3.isResolved()).to.equal(false)
-
-    expect(status1.isRejected()).to.equal(false)
-    expect(status2.isRejected()).to.equal(false)
-    expect(status3.isRejected()).to.equal(true)
-
-    expect(status1.value()).to.equal(42)
-    expect(status2.value()).to.equal(Math.PI)
-    expect(::status3.value).to.throw()
-
-    expect(::status1.reason).to.throw()
-    expect(::status2.reason).to.throw()
-    expect(status3.reason()).to.equal('fatality')
   })
 })
