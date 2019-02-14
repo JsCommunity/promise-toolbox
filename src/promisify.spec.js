288e1da742bfcfeb37fa0e88ea417a01b8dc9682
Julien Fontanet <julien.fontanet@isonoe.net>
2019-02-14 13:13:15 +0100

    chore(promisify): add tests

---
+++ src/promisify.spec.js
@@ -0,0 +1,53 @@
+/* eslint-env jest */
+
+const promisify = require('./promisify')
+
+describe('promisify()', () => {
+  it('handle callback results', async () => {
+    const value = {}
+    expect(
+      await promisify(function (cb) {
+        cb(null, value)
+      })()
+    ).toBe(value)
+  })
+
+  it('handle callback errors', async () => {
+    const error = new Error()
+    await expect(
+      promisify(function (cb) {
+        cb(error)
+      })()
+    ).rejects.toThrowError(error)
+  })
+
+  it('handle thrown values', async () => {
+    const error = new Error()
+    await expect(
+      promisify(function () {
+        throw error
+      })()
+    ).rejects.toThrowError(error)
+  })
+
+  it('forwards context and arguments', () => {
+    const thisArg = {}
+    const args = [ {}, {} ]
+    promisify(function () {
+      expect(this).toBe(thisArg)
+      expect([].slice.call(arguments, 0, -1)).toEqual(args)
+    }).apply(thisArg, args)
+  })
+
+  it('returns a function with the same name', () => {
+    function foo () {}
+    expect(promisify(foo).name).toBe(foo.name)
+  })
+
+  it('returns a function with one less param', () => {
+    expect(promisify(function (a, b) {}).length).toBe(1)
+
+    // special case if fn has no param
+    expect(promisify(function () {}).length).toBe(0)
+  })
+})
