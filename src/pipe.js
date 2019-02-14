39cdcc00f8154b6a05b879fcb815333632ad5ae5
Julien Fontanet <julien.fontanet@isonoe.net>
2019-02-14 14:40:05 +0100

    chore(pipe): optimize by moving reducer outside

--- src/pipe.js
+++ src/pipe.js
@@ -1,13 +1,11 @@
 const { isArray, prototype: { slice } } = Array
 
+const chain = (promise, fn) => promise.then(fn)
+
 module.exports = function pPipe (fns) {
   if (!isArray(fns)) {
     fns = slice.call(arguments)
   }
 
-  return function (arg) {
-    return fns.reduce(function (prev, fn) {
-      return prev.then(fn)
-    }, Promise.resolve(arg))
-  }
+  return arg => fns.reduce(chain, Promise.resolve(arg))
 }
