13fcb6d2b958682f18533850b4c733dba5a8ee86
Julien Fontanet <julien.fontanet@isonoe.net>
2019-03-19 17:15:27 +0100

    chore(retry): reimplement without async/await
    Fix`regeneratorRuntime is not defined`.

--- src/retry.js
+++ src/retry.js
@@ -1,35 +1,40 @@
 const matchError = require("./_matchError");
 const noop = require("./_noop");
 
-const pDelay = require("./delay");
-
 function stopRetry(error) {
   this.error = error;
   // eslint-disable-next-line no-throw-literal
   throw this;
 }
 
-module.exports = async function retry(
+module.exports = function retry(
   fn,
   { delay = 1e3, onRetry = noop, tries = 10, when } = {}
 ) {
   const container = { error: undefined };
   const stop = stopRetry.bind(container);
 
+  let sleep;
+  if (delay !== 0) {
+    const resolver = resolve => setTimeout(resolve, delay);
+    sleep = () => new Promise(resolver);
+  }
+
   when = matchError.bind(undefined, when);
 
-  while (true) {
-    try {
-      return await fn(stop);
-    } catch (error) {
-      if (error === container) {
-        throw container.error;
-      }
-      if (--tries === 0 || !when(error)) {
-        throw error;
-      }
-      await onRetry(error);
+  const onError = error => {
+    if (error === container) {
+      throw container.error;
     }
-    await pDelay(delay);
-  }
+    if (--tries === 0 || !when(error)) {
+      throw error;
+    }
+    return Promise.resolve(onRetry(error))
+      .then(sleep)
+      .then(loop);
+  };
+  const resolver = resolve => resolve(fn(stop));
+  const loop = () => new Promise(resolver).catch(onError);
+
+  return loop();
 };
