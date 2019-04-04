ee249c93ddc00966aa6b7d610a03ad0449e16737
Julien Fontanet <julien.fontanet@isonoe.net>
2019-04-04 09:56:18 +0200

    chore(timeout): dont create error if ms is 0

--- src/timeout.js
+++ src/timeout.js
@@ -6,9 +6,13 @@ const TimeoutError = require("./TimeoutError");
 // - promise::timeout(ms, cb)
 //
 // 0 is a special value that disable the timeout
-module.exports = function timeout(ms, onReject = new TimeoutError()) {
+module.exports = function timeout(ms, onReject) {
   if (ms === 0) {
     return this;
+  }
+
+  if (onReject === undefined) {
+    onReject = new TimeoutError();
   }
 
   return new Promise((resolve, reject) => {
